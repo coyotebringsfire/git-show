4e6d69e337b333dafeecd42134427f7a183f1b31
CoyoteBringsFire <aumkara@gmail.com>
2015-06-02 15:05:31 -0700

    initial commit + tests

---
+++ git_show.js
@@ -0,0 +1,168 @@
+"use strict";
+var async=require('async');
+
+var Git=require('git-wrapper2'),
+	ss=require('simple-statistics'),
+	Q=require('q');
+
+function GitShow(commit) {
+	var debug=require('debug')('git:GitShow'),
+		git=new Git(), gitExecPromise=Q.defer();
+
+	debug("showing commit %j", commit);
+	git.exec("show", [commit], function results(err, show_out) {
+		debug=require('debug')('git:GitShow:results');
+		if(err) {
+			debug("err: %j", err);
+			return gitExecPromise.reject(err);
+		}
+		if( show_out==undefined ) {
+			return gitExecPromise.resolve(null);
+		}
+		debug("show output: %s", typeof show_out);
+		var out=show_out.split('\n'), currentDiff, allDiffs=[], showPromise=Q.defer(),
+			commit={
+				hash: undefined,
+				author: undefined,
+				date: undefined,
+				message: undefined
+			};
+		
+		async.eachSeries( out, function forEachLine(line, nextLine) {
+			debug=require('debug')('git:GitShow:results:forEachLine');
+			debug("line %s", line);
+			if( allDiffs.length===0 ) {
+				if( commit.hash===undefined) {
+					debug("setting hash");
+					commit.hash=line;
+					nextLine();
+				}
+				if( commit.author===undefined) {
+					debug("setting author");
+					commit.author=line;
+					nextLine();
+				}
+				if( commit.date===undefined) {
+					debug("setting date");
+					commit.date=line;
+					nextLine();
+				}
+				if(line==="") {
+					if(commit.message) {
+						debug("setting blank line");
+						commit.message+="\n";
+						nextLine();
+					}
+				}
+				if( commit.message===undefined) {
+					debug("setting message");
+					commit.message=line;
+					nextLine();
+				} 
+			}
+			if( currentDiff === undefined ) {
+				var match=line.match(/^diff --git a\/([^ ]+) b\/(.+)/);
+				if( match ) {
+					debug("new diff %s %s", match[1], match[2]);
+					currentDiff={
+						linesAdded: 0,
+						linesDeleted: 0,
+						deltas: 0,
+						fileOne: match[1],
+						fileTwo: match[2]
+					};
+					nextLine();
+				}
+			} else {
+				//look for new file line
+				debug("looking for new file");
+				var newFileLineMatch=line.match(/new file mode ([a-zA-Z0-9]{6})/);
+				if( newFileLineMatch ){
+					nextLine();
+				}
+				//look for index line
+				var indexLineMatch=line.match(/^index ([a-zA-Z0-9]+)\.\.([a-zA-Z0-9]+) ([0-7]{6})/), 
+					plusLine, minusLine, newDiffMatch; 
+				debug("looking for index");
+				if( match ) {
+					debug("index line: %s %s %s", indexLineMatch[1], indexLineMatch[2], indexLineMatch[3]);
+					currentDiff.startingHash=indexLineMatch[1];
+					currentDiff.endingHash=indexLineMatch[2];
+					currentDiff.mode=indexLineMatch[3];
+					nextLine();
+				}
+				//look for +++ line
+				plusLine=line.match(/^\+{3} a\/[.+]/);
+				debug("looking for +++");
+				if( plusLine ) {
+					debug("+++");
+					nextLine();
+				}
+				//look for --- line
+				minusLine=line.match(/^-{3} b\/[.+]/);
+				debug("looking for ---");
+				if( minusLine ) {
+					debug("---");
+					nextLine();
+				}
+				//look for the line
+				debug("looking for + or - in %s", line);
+				if(line.match(/^-/)) {
+					currentDiff.linesDeleted++;
+					debug("one more line deleted %d", currentDiff.linesDeleted);
+				}
+				if(line.match(/^\+/)) {
+					currentDiff.linesAdded++;
+					debug("one more line added %d", currentDiff.linesAdded);
+				}
+				newDiffMatch=line.match(/^diff --git a\/([^ ]+) b\/(.+)/);
+				debug("looking for new diff in %s", line);
+				if(newDiffMatch) {
+					currentDiff.deltas=currentDiff.linesAdded-currentDiff.linesDeleted;
+					allDiffs.push(currentDiff);
+					currentDiff={
+						linesAdded: 0,
+						linesDeleted: 0,
+						deltas: 0,
+						fileOne: newDiffMatch[1],
+						fileTwo: newDiffMatch[2]
+					};
+				}
+				nextLine();
+			}
+		}, function afterEveryLineIsProcessed(err) {
+			debug=require('debug')('git:GitShow:results:afterEveryLineIsProcessed');
+			var numDiffs=allDiffs.length, allDeltas=[],
+				totalLinesChanged=0, averageLinesChanged=0, varianceLinesChanged=0, standardDeviationLinesChanged=0;
+
+			debug("all lines processed");
+			async.each( allDiffs, function eachDiff(d, done) {
+				debug=require('debug')('git:GitShow:results:afterEveryLineIsProcessed:eachDiff');
+				debug("adding diff");
+				totalLinesChanged+=d.deltas;
+				allDeltas.push(d.deltas);
+				done();
+			}, function afterTotalLinesCalculated(err) {
+				debug=require('debug')('git:GitShow:results:afterEveryLineIsProcessed:afterTotalLinesCalculated');
+				if(err) {
+					debug("err: %j", err);
+					return gitExecPromise.reject(err);
+				}
+				averageLinesChanged=ss.mean(allDeltas);
+				varianceLinesChanged=ss.variance(allDeltas);
+				standardDeviationLinesChanged=ss.standard_deviation(allDeltas);
+				debug("resolving promise %j %d %d %d", allDeltas, averageLinesChanged, varianceLinesChanged, standardDeviationLinesChanged );
+				
+				gitExecPromise.resolve({
+					averageLinesChanged: averageLinesChanged,
+					varianceLinesChanged: varianceLinesChanged,
+					standardDeviationLinesChanged: standardDeviationLinesChanged,
+					diffs: allDiffs
+				});
+			});
+		});
+	});
+	return gitExecPromise.promise;
+}
+
+module.exports=GitShow;
---
+++ test/git_show.js
@@ -0,0 +1,214 @@
+var should=require('should'),
+	gitShow=require('../git_show.js'),
+	testRepo="/Users/aumkara/workspace/MuMoo",
+	debug;
+
+describe('git-show', function gitShowLib() {
+	debug=require('debug')('git:test:gitShowLib');
+	before(function chdirTestrepo(done) {
+		debug=require('debug')('git:test:gitShowLib:chdirTestrepo');
+		process.chdir(testRepo);
+		done();
+	});
+	it("should return a promise", function test(done) {
+		debug=require('debug')('git:test:gitShowLib:test');
+		var returnObject=gitShow();
+		//duck-type check the returned object to see if it's thennable
+		returnObject.then.should.be.type('function');
+		
+		done();
+	});
+	it("should reject promise for invalid commit hash", function test(done) {
+		debug=require('debug')('git:test:gitShowLib:test');
+		gitShow('invalidhash').
+			then(function onResolve(output) {
+				debug=require('debug')('git:test:gitShowLib:test:onResolve');
+				should.fail();
+				done();
+			}, function onReject(err) {
+				debug=require('debug')('git:test:gitShowLib:test:onReject');
+				debug("err: %j", err);
+				err.should.match(/unknown revision or path not in the working tree/);
+				done();
+			});
+	});
+	it("should resolve promise for HEAD if not passed a commit hash", function test(done) {
+		debug=require('debug')('git:test:gitShowLib:test');
+		gitShow().
+			then(function onResolve(output) {
+				debug=require('debug')('git:test:gitShowLib:test:onResolve');
+				debug("output: %j", output);
+				done();
+			}, function onReject(err) {
+				debug=require('debug')('git:test:gitShowLib:test:onReject');
+				should.fail(err);
+				done();
+			});
+	});
+	it("should resolve promise for specified commit hash", function test(done) {
+		debug=require('debug')('git:test:gitShowLib:test');
+		gitShow('HEAD').
+			then(function onResolve(output) {
+				debug=require('debug')('git:test:gitShowLib:test:onResolve');
+				output.should.be.ok;
+				done();
+			}, function onReject(err) {
+				debug=require('debug')('git:test:gitShowLib:test:onReject');
+				should.fail(err);
+				done();
+			});
+	});
+	describe("results", function() {
+		it("should contain average lines changed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.averageLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain variance lines changed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.varianceLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain standard deviation lines changed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.standardDeviationLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});
+		it("should contain average lines added", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.averageLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain variance lines added", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.varianceLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain standard deviation lines added", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.standardDeviationLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain average lines removed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.averageLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain variance lines removed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.varianceLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain standard deviation lines removed", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.standardDeviationLinesChanged.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+		it("should contain all diffs", function test(done) {
+			debug=require('debug')('git:test:gitShowLib:results:test');
+			gitShow('HEAD').
+				then(function onResolve(output) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onResolve');
+					debug("output: %j", output);
+					output.diffs.should.not.equal(undefined);
+					done();
+				}, function onReject(err) {
+					debug=require('debug')('git:test:gitShowLib:results:test:onReject');
+					debug("err %j", err);
+					should.fail(err);
+					done();
+				});
+		});	
+	});
+	
+});
